360124bcbd56517c12429aa8a6a301e9164e5203
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-05-15 19:08:36 +0200

    added new files

--- Planiranje/Planiranje/App_Start/RouteConfig.cs
+++ Planiranje/Planiranje/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Planiranje
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Planiranje", action = "Prijava", id = UrlParameter.Optional }
             );
         }
     }
