47b008ece72899c298a15c47e49030b1312f647b
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-05-18 19:37:16 +0200

    web connected to database

--- Planiranje/Planiranje/Baza/BazaPodataka.cs
+++ Planiranje/Planiranje/Baza/BazaPodataka.cs
@@ -16,6 +16,5 @@ namespace Planiranje.Controllers
 		{
 			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 		}
-
 	}
 }
--- Planiranje/Planiranje/Controllers/PlaniranjeController.cs
+++ Planiranje/Planiranje/Controllers/PlaniranjeController.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Planiranje.Models;
 
 namespace Planiranje.Controllers
 {
     public class PlaniranjeController : Controller
     {
-		// GET: Planiranje
-		public ActionResult Index()
-		{
-			return View();
-		}
+		private BazaPodataka baza = new BazaPodataka();
+
+		[HttpGet]
 		public ActionResult Prijava()
+		{
+			Pedagog ped = new Pedagog();
+			return View(ped);
+		}
+
+		[HttpPost]
+		public ActionResult Prijava(Pedagog p)
+		{
+			foreach (Pedagog pedagog in baza.Pedagog)
+			{
+				if (pedagog.Lozinka == p.Lozinka && pedagog.Email == p.Email)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+			return RedirectToAction("Prijava");
+		}
+		public ActionResult Index()
 		{
 			return View();
 		}
