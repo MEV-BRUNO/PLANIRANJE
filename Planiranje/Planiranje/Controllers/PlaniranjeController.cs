0e1acb59d5ecff6e35f4cc6a059a90f905f3b5e9
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-05-23 19:29:18 +0200

    updated titles

--- Planiranje/Planiranje/Controllers/PlaniranjeController.cs
+++ Planiranje/Planiranje/Controllers/PlaniranjeController.cs
@@ -15,6 +15,7 @@ namespace Planiranje.Controllers
 		public ActionResult Prijava()
 		{
 			AppSession.Current.UserId = 0;
+			ViewBag.Title = "Prijava";
 			return View();
 		}
 
@@ -36,6 +37,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Pocetna";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -44,6 +46,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Zaboravljena lozinka";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -52,6 +55,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Registracija";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -61,6 +65,7 @@ namespace Planiranje.Controllers
 			if (AppSession.Current.UserId > 0)
 			{
 				var mjesecni_p = mjesecni_planovi.DohvatiMjesecnePlanove().ToList();
+				ViewBag.Title = "Mjesecni plan";
 				return View(mjesecni_p);
 			}
 			return RedirectToAction("Prijava");
@@ -69,6 +74,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Godisnji plan";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -77,6 +83,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Podrucja djelovanja";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -85,6 +92,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Aktivnosti";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -93,6 +101,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Akcije";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -101,6 +110,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Oblici i metode";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -109,6 +119,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Subjekti";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -117,6 +128,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Zadaci";
 				return View();
 			}
 			return RedirectToAction("Prijava");
@@ -125,6 +137,7 @@ namespace Planiranje.Controllers
 		{
 			if (AppSession.Current.UserId > 0)
 			{
+				ViewBag.Title = "Ciljevi";
 				return View();
 			}
 			return RedirectToAction("Prijava");
