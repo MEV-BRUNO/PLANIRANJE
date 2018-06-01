a8f6adbc6dfb167451fd5bfcf6eba462b6b60e5a
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-06-01 15:10:55 +0200

    updated dbHandle

--- Planiranje/Planiranje/Models/Mjesecni_plan_DBHandle.cs
+++ Planiranje/Planiranje/Models/Mjesecni_plan_DBHandle.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using MySql.Data.MySqlClient;
 using Planiranje.Controllers;
+using PagedList;
 
 namespace Planiranje.Models
 {
@@ -53,6 +54,42 @@ namespace Planiranje.Models
 			return planovi;
 		}
 
+		public List<Mjesecni_plan> DohvatiMjesecnePlanove(string searchData)
+		{
+			connection();
+			List<Mjesecni_plan> planovi = new List<Mjesecni_plan>();
+			con.ConnectionString = str;
+			using (MySqlCommand cmd = new MySqlCommand())
+			{
+				cmd.Connection = con;
+				cmd.CommandText = "SELECT id_plan, naziv, ak_godina, opis " +
+					"FROM mjesecni_plan where id_pedagog = " + PlaniranjeSession.Trenutni.PedagogId + " and" +
+					" naziv like '%" + searchData + "%' " +
+					"ORDER BY id_plan ASC";
+				con.Open();
+				using (MySqlDataReader sdr = cmd.ExecuteReader())
+				{
+					if (sdr.HasRows)
+					{
+						while (sdr.Read())
+						{
+							Mjesecni_plan plan = new Mjesecni_plan()
+							{
+								ID_plan = Convert.ToInt32(sdr["id_plan"]),
+								Naziv = sdr["naziv"].ToString(),
+								Ak_godina = sdr["ak_godina"].ToString(),
+								Opis = sdr["opis"].ToString(),
+							};
+							if (plan.Naziv.Length > 0)
+								planovi.Add(plan);
+						}
+					}
+				}
+				con.Close();
+			}
+			return planovi;
+		}
+
 		public Mjesecni_plan DohvatiMjesecniPlan(int _id)
 		{
 			connection();
@@ -61,13 +98,13 @@ namespace Planiranje.Models
 			using (MySqlCommand cmd = new MySqlCommand())
 			{
 				cmd.Connection = con;
-				cmd.CommandText = "SELECT * FROM mjesecni_plan WHERE id_plan = 1";
+				cmd.CommandText = "SELECT * FROM mjesecni_plan WHERE id_plan = @id";
 				cmd.CommandType = CommandType.Text;
 				cmd.Parameters.AddWithValue("@id", _id);
 				con.Open();
 				using (MySqlDataReader sdr = cmd.ExecuteReader())
 				{
-					if (sdr.HasRows && sdr.FieldCount == 1)
+					if (sdr.HasRows)
 					{
 						while (sdr.Read())
 						{
@@ -113,6 +150,84 @@ namespace Planiranje.Models
 				con.Close();
 			}
 			return result;
+		}
+
+		public string DodajMjesecniPlan(Mjesecni_plan plan)
+		{
+			try
+			{
+				connection();
+				using (MySqlCommand cmd = new MySqlCommand())
+				{
+					cmd.Connection = con;
+					cmd.CommandText = "INSERT INTO grad(id,naziv,zupanija) " +
+						" VALUES(@id,@naziv,@zupanija)";
+					cmd.CommandType = CommandType.Text;
+					cmd.Parameters.AddWithValue("@id", plan.ID_pedagog);
+					cmd.Parameters.AddWithValue("@naziv", plan.Naziv);
+					cmd.Parameters.AddWithValue("@zupanija", plan.Opis);
+					con.Open();
+					cmd.ExecuteNonQuery();
+					con.Close();
+					return "OK";
+				}
+			}
+			catch (Exception err)
+			{
+				con.Close();
+				return "Error" + err.Message.ToString();
+			}
+		}
+
+		public string deleteGrad(int _id)
+		{
+			try
+			{
+				connection();
+				using (MySqlCommand cmd = new MySqlCommand())
+				{
+					cmd.Connection = con;
+					con.Open();
+
+					cmd.CommandText = "DELETE FROM mjesecni_plan WHERE id_plan = @id";
+					cmd.CommandType = CommandType.Text;
+					cmd.Parameters.AddWithValue("@id", _id);
+					cmd.ExecuteNonQuery();
+					con.Close();
+					return "OK";
+				}
+			}
+			catch (Exception err)
+			{
+				con.Close();
+				return "Error";
+			}
+		}
+
+		public string updateGrad(Mjesecni_plan data)
+		{
+			try
+			{
+				connection();
+				using (MySqlCommand cmd = new MySqlCommand())
+				{
+					cmd.Connection = con;
+					cmd.CommandText = "UPDATE mjesecni_plan SET naziv = @naziv, opis = @opis  WHERE id_plan = @id";
+					cmd.CommandType = CommandType.Text;
+					cmd.Parameters.AddWithValue("@id", data.ID_plan);
+					cmd.Parameters.AddWithValue("@naziv", data.Naziv);
+					cmd.Parameters.AddWithValue("@opis", data.Opis);
+					con.Open();
+					cmd.ExecuteNonQuery();
+					con.Close();
+					return "OK";
+				}
+			}
+			catch (Exception err)
+			{
+				con.Close();
+				return "Error";
+			}
 		}
 	}
 }
