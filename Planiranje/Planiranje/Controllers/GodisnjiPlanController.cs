64abfc8232d22577fe6a58b9e22cb732b9e2b580
David <david.sopar@student.mev.hr>
2018-06-07 21:11:27 +0200

    dodan 1/4 view-a, dbhandle i nadograđeni controller; sve za godisnji_plan

--- Planiranje/Planiranje/Controllers/GodisnjiPlanController.cs
+++ Planiranje/Planiranje/Controllers/GodisnjiPlanController.cs
@@ -1,34 +1,180 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using Planiranje.Models;
 
 namespace Planiranje.Controllers
 {
     public class GodisnjiPlanController : Controller
 	{
-		// GET: GodisnjiPlan
-		public ActionResult Index()
+        private Godisnji_plan_DBHandle godisnji_planovi = new Godisnji_plan_DBHandle();     
+        int Page_No_Master = 1;
+        // GET: GodisnjiPlan
+        public ActionResult Index(string Sort, string Search, string Filter, int? Page_No)
         {
-			if (PlaniranjeSession.Trenutni.PedagogId > 0)
-			{
-                List<Godisnji_plan> lista = new List<Godisnji_plan>();
-                Godisnji_plan g = new Godisnji_plan
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            ViewBag.Title = "Pregled godisnjih planova";
+            ViewBag.CurrentSortOrder = Sort;
+            ViewBag.SortingName = String.IsNullOrEmpty(Sort) ? "Naziv" : "";
+
+            ViewBag.Message = "Grad";
+
+            ViewBag.FilterValue = Search;
+            if (Search != null)
+            {
+                Page_No = 1;
+            }
+            else
+            {
+                Search = Filter;
+            }
+            ViewBag.CurrentPage = 1;
+            if (Page_No != null)
+                ViewBag.CurrentPage = Page_No;
+
+
+            int Size_Of_Page = 10;
+            int No_Of_Page = (Page_No ?? 1);
+            if (Search == null || Search.Length == 0)
+            {
+
+                if (Request.IsAjaxRequest())
                 {
-                    Ak_godina = "2017/2018",
-                    Br_dana_godina_odmor = 4,
-                    God_fond_sati = 450,
-                    Id_god = 3,
-                    Id_pedagog = PlaniranjeSession.Trenutni.PedagogId,
-                    Br_radnih_dana = 40,
-                    Ukupni_rad_dana = 66
-                };
-                lista.Add(g);
-				return View(lista);
-			}
-			return RedirectToAction("Prijava");
-		}        
+                    int noP = (int)Page_No_Master;
+                    var Popis2 = godisnji_planovi.ReadGodisnjePlanove().ToPagedList(No_Of_Page, Size_Of_Page);
+                    return PartialView("_GradView", Popis2);
+                }
+                Page_No_Master = No_Of_Page;
+                var Popis = godisnji_planovi.ReadGodisnjePlanove().ToPagedList(No_Of_Page, Size_Of_Page);
+                return View(Popis);
+            }
+            else
+            {
+                Page_No_Master = No_Of_Page;
+                var Popis = godisnji_planovi.ReadGodisnjePlanove(Search).ToPagedList(No_Of_Page, Size_Of_Page);
+                if (Request.IsAjaxRequest())
+                {
+                    return PartialView("_GradView", Popis);
+                }
+
+                return View(Popis);
+            }
+
+        }
+        public ActionResult NoviPlan()
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            if (Request.IsAjaxRequest())
+            {
+                ViewBag.IsUpdate = false;
+                return View("NoviPlan");
+            }
+            return View("NoviPlan");
+        }
+
+        [HttpPost]
+        public ActionResult NoviPlan(Godisnji_plan gr)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            Godisnji_plan godisnji_plan = new Godisnji_plan();
+            godisnji_plan.Id_pedagog = PlaniranjeSession.Trenutni.PedagogId;
+            godisnji_plan.Ak_godina = gr.Ak_godina;
+            godisnji_plan.Br_dana_godina_odmor = gr.Br_dana_godina_odmor;
+            godisnji_plan.Br_radnih_dana = gr.Br_radnih_dana;
+            godisnji_plan.God_fond_sati = gr.God_fond_sati;
+            godisnji_plan.Ukupni_rad_dana = gr.Ukupni_rad_dana;
+            godisnji_plan.Id_god = gr.Id_god;
+            
+            if (!godisnji_planovi.CreateGodisnjiPlan(godisnji_plan))
+            {
+                ModelState.Clear();
+                return PartialView("NoviPlan", gr);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            Godisnji_plan godisnji_plan = new Godisnji_plan();
+            godisnji_plan = godisnji_planovi.ReadGodisnjiPlan(id);
+            if (Request.IsAjaxRequest())
+            {
+                ViewBag.IsUpdate = false;
+                return View("Uredi", godisnji_plan);
+            }
+            return View("Uredi", godisnji_plan);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Godisnji_plan godisnji_plan)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            if (!godisnji_planovi.UpdateGodisnjiPlan(godisnji_plan))
+            {
+                ModelState.Clear();
+                return PartialView("Uredi", godisnji_plan);
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            Godisnji_plan godisnji_plan = new Godisnji_plan();
+            godisnji_plan = godisnji_planovi.ReadGodisnjiPlan(id);
+            if (Request.IsAjaxRequest())
+            {
+                ViewBag.IsUpdate = false;
+                return View("Obrisi", godisnji_plan);
+            }
+            return View("Obrisi");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Godisnji_plan godisnji_plan)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            if (!godisnji_planovi.DeleteGodisnjiPlan(godisnji_plan.Id_god))
+            {
+                ModelState.Clear();
+                return PartialView("Obrisi");
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Planiranje/Planiranje/Models/Godisnji_plan_DBHandle.cs
@@ -0,0 +1,258 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+using System.Configuration;
+using Planiranje.Controllers;
+using System.Data;
+
+namespace Planiranje.Models
+{
+    public class Godisnji_plan_DBHandle
+    {
+        private MySqlConnection connection;
+
+        private void Connect()
+        {
+            string connection_string = ConfigurationManager.ConnectionStrings["BazaPodataka"].ConnectionString;
+            connection = new MySqlConnection(connection_string);
+        }
+
+        public List<Godisnji_plan> ReadGodisnjePlanove()
+        {
+            List<Godisnji_plan> godisnji_planovi = new List<Godisnji_plan>();
+            this.Connect();
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT id_god, id_pedagog, ak_godina, br_radnih_dana, br_dana_godina_odmor, ukupni_rad_dana, god_fond_sati" +
+                    "FROM godisnji_plan " +
+                    "WHERE id_pedagog = @id_pedagog " +
+                    "ORDER BY id_god ASC";
+                command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                connection.Open();
+                using (MySqlDataReader sdr = command.ExecuteReader())
+                {
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            Godisnji_plan plan = new Godisnji_plan()
+                            {
+                                //Id_pedagog = Convert.ToInt32(sdr["id_pedagog"]),
+                                Id_god = Convert.ToInt32(sdr["id_god"]),
+                                //Naziv = sdr["naziv"].ToString(),
+                                Ak_godina = sdr["ak_godina"].ToString(),
+                                //Opis = sdr["opis"].ToString(),
+                                Br_radnih_dana = Convert.ToInt32(sdr["br_radnih_dana"]),
+                                Br_dana_godina_odmor = Convert.ToInt32(sdr["br_dana_godina_odmor"]),
+                                Ukupni_rad_dana = Convert.ToInt32(sdr["ukupni_rad_dana"]),
+                                God_fond_sati = Convert.ToInt32(sdr["god_fond_sati"]),
+                            };
+                            godisnji_planovi.Add(plan);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return godisnji_planovi;
+        }
+
+        public List<Godisnji_plan> ReadGodisnjePlanove(string search_string)
+        {
+            List<Godisnji_plan> godisnji_planovi = new List<Godisnji_plan>();
+            this.Connect();
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = connection;
+                //command.CommandText = "SELECT id_plan, ak_godina, naziv, opis " +
+                //    "FROM mjesecni_plan " +
+                //    "WHERE id_pedagog = @id_pedagog " +
+                //    "AND (ak_godina like '%" + search_string + "%' " +
+                //    "OR naziv like '%" + search_string + "%' " +
+                //    "OR opis like '%" + search_string + "%') " +
+                //    "ORDER BY id_plan ASC";
+                command.CommandText = "SELECT id_god, ak_godina, br_radnih_dana, br_dana_godina_odmor, ukupni_rad_dana, god_fond_sati" +
+                    "FROM godisnji_plan " +
+                    "WHERE id_pedagog = @id_pedagog " +
+                    "AND (ak_godina like '%" + search_string + "%' " +
+                    "OR br_radnih_dana like '%" + search_string +"%')"+
+                    "ORDER BY id_god ASC";
+                command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                connection.Open();
+                using (MySqlDataReader sdr = command.ExecuteReader())
+                {
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            Godisnji_plan plan = new Godisnji_plan()
+                            {
+                                Id_god = Convert.ToInt32(sdr["id_god"]),
+                                //Naziv = sdr["naziv"].ToString(),
+                                Ak_godina = sdr["ak_godina"].ToString(),
+                                //Opis = sdr["opis"].ToString(),
+                                Br_radnih_dana = Convert.ToInt32(sdr["br_radnih_dana"]),
+                                Br_dana_godina_odmor = Convert.ToInt32(sdr["br_dana_godina_odmor"]),
+                                Ukupni_rad_dana = Convert.ToInt32(sdr["ukupni_rad_dana"]),
+                                God_fond_sati = Convert.ToInt32(sdr["god_fond_sati"]),
+                            };
+                            godisnji_planovi.Add(plan);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return godisnji_planovi;
+        }
+
+        public Godisnji_plan ReadGodisnjiPlan(int _id)
+        {
+            Godisnji_plan godisnji_plan = new Godisnji_plan();
+            this.Connect();
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT id_god, id_pedagog, ak_godina, br_radnih_dana, br_dana_godina_odmor, ukupni_rad_dana, god_fond_sati" +
+                    "FROM godisnji_plan " +
+                    "WHERE id_pedagog = @id_pedagog " +
+                    "AND id_god = @id "+
+                    "ORDER BY id_god ASC";
+                command.CommandType = CommandType.Text;
+                command.Parameters.AddWithValue("@id", _id);
+                command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                connection.Open();
+                using (MySqlDataReader sdr = command.ExecuteReader())
+                {
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            godisnji_plan = new Godisnji_plan()
+                            {
+                                Id_god = Convert.ToInt32(sdr["id_god"]),
+                                //Naziv = sdr["naziv"].ToString(),
+                                Ak_godina = sdr["ak_godina"].ToString(),
+                                //Opis = sdr["opis"].ToString(),
+                                Br_radnih_dana = Convert.ToInt32(sdr["br_radnih_dana"]),
+                                Br_dana_godina_odmor = Convert.ToInt32(sdr["br_dana_godina_odmor"]),
+                                Ukupni_rad_dana = Convert.ToInt32(sdr["ukupni_rad_dana"]),
+                                God_fond_sati = Convert.ToInt32(sdr["god_fond_sati"]),
+                            };
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return godisnji_plan;
+        }
+
+        public bool CreateGodisnjiPlan(Godisnji_plan godisnji_plan)
+        {
+            try
+            {
+                this.Connect();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "INSERT INTO godisnji_plan " +
+                        "(id_god, id_pedagog, ak_godina, br_radnih_dana, br_dana_godina_odmor, ukupni_rad_dana, god_fond_sati) " +
+                        " VALUES (@id_god, @id_pedagog, @ak_godina, @br_radnih_dana, @br_dana_godina_odmor, @ukupni_rad_dana, @god_fond_sati)";
+                    command.CommandType = CommandType.Text;
+                    command.Parameters.AddWithValue("@id_god", godisnji_plan.Id_god);
+                    command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                    command.Parameters.AddWithValue("@ak_godina", godisnji_plan.Ak_godina);
+                    command.Parameters.AddWithValue("@br_radnih_dana", godisnji_plan.Br_radnih_dana);
+                    command.Parameters.AddWithValue("@br_dana_godina_odmor", godisnji_plan.Br_dana_godina_odmor);
+                    command.Parameters.AddWithValue("@ukupni_rad_dana", godisnji_plan.Ukupni_rad_dana);
+                    command.Parameters.AddWithValue("@god_fond_sati", godisnji_plan.God_fond_sati);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+
+        public bool UpdateGodisnjiPlan(Godisnji_plan godisnji_plan)
+        {
+            try
+            {
+                this.Connect();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "UPDATE godisnji_plan " +
+                        "SET " +
+                        "ak_godina = @ak_godina, " +
+                        "br_radnih_dana = @br_radnih_dana, " +
+                        "br_dana_godina_odmor = @br_dana_godina_odmor " +
+                        "ukupni_rad_dana = @ukupni_rad_dana" +
+                        "god_fond_sati = @god_fond_sati"+
+                        "WHERE id_god = @id_god " +
+                        "AND id_pedagog = @id_pedagog";
+                    command.CommandType = CommandType.Text;
+                    command.Parameters.AddWithValue("@id_god", godisnji_plan.Id_god);
+                    command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                    command.Parameters.AddWithValue("@ak_godina", godisnji_plan.Ak_godina);
+                    command.Parameters.AddWithValue("@br_radnih_dana", godisnji_plan.Br_radnih_dana);
+                    command.Parameters.AddWithValue("@br_dana_godina_odmor", godisnji_plan.Br_dana_godina_odmor);
+                    command.Parameters.AddWithValue("@ukupni_rad_dana", godisnji_plan.Ukupni_rad_dana);
+                    command.Parameters.AddWithValue("@god_fond_sati", godisnji_plan.God_fond_sati);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+
+        public bool DeleteGodisnjiPlan(int id)
+        {
+            try
+            {
+                this.Connect();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    connection.Open();
+                    command.CommandText = "DELETE FROM godisnji_plan " +
+                        "WHERE id_god = @id_god " +
+                        "AND id_pedagog = @id_pedagog";
+                    command.CommandType = CommandType.Text;
+                    command.Parameters.AddWithValue("@id_god", id);
+                    command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+    }
+}
