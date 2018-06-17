596431ac52de48f505d2c8920133ce325ace05d8
David <david.sopar@student.mev.hr>
2018-06-17 19:41:47 +0200

    Omogućena funkcija zaboravljene lozinke

--- Planiranje/Planiranje/Controllers/PlaniranjeController.cs
+++ Planiranje/Planiranje/Controllers/PlaniranjeController.cs
@@ -3,12 +3,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Planiranje.Models;
+using System.Web.Security;
+using System.Runtime.Remoting.Messaging;
+using System.Net.Mail;
+
 
 namespace Planiranje.Controllers
 {
 	public class PlaniranjeController : Controller
     {
 		private BazaPodataka baza = new BazaPodataka();
+        private Pedagog_DBHandle pedagog_db = new Pedagog_DBHandle();
 
 		[HttpGet]
 		public ActionResult Prijava()
@@ -50,6 +55,47 @@ namespace Planiranje.Controllers
 			}
 			return RedirectToAction("Prijava", "Planiranje");
 		}
+
+        [HttpPost]
+        public ActionResult ZaboravljenaLozinka(Pedagog p)
+        {
+            Pedagog pedagog = baza.Pedagog.SingleOrDefault(ped => ped.Email == p.Email);
+            if (pedagog == null)
+            {
+                ViewBag.Title = "Zaboravljena lozinka";
+                return View();
+            }
+            string[] abeceda = { "a","b", "c", "d", "e", "f", "g" };
+            Random r = new Random();
+            string lozinka="";
+            for(int i = 0; i < 6; i++)
+            {
+                lozinka += abeceda[r.Next(0, abeceda.Length-1)];
+            }
+            pedagog.Lozinka = lozinka;
+            if (pedagog_db.UpdatePedagog(pedagog))
+            {
+                MailMessage mail = new MailMessage("noreply@planiranje.com", pedagog.Email,
+                    "Podaci o promjeni lozinke", "Vaša nova lozinka je " + lozinka + ". Molimo promijenite je u što kraćem roku.");
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                try
+                {
+                    smtp.Send(mail);
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                ViewBag.Title = "Zaboravljena lozinka";
+                return View();
+            }
+
+            baza.SaveChanges();
+            return View("Prijava");
+        }
 		public ActionResult Registracija()
 		{
 			if (PlaniranjeSession.Trenutni.PedagogId == 0)
---
+++ Planiranje/Planiranje/Models/Pedagog_DBHandle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using Planiranje.Controllers;
+using PagedList;
+
+namespace Planiranje.Models
+{
+    public class Pedagog_DBHandle
+    {
+        private MySqlConnection connection;
+
+        private void Connect()
+        {
+            string connection_string = ConfigurationManager.ConnectionStrings["BazaPodataka"].ConnectionString;
+            connection = new MySqlConnection(connection_string);
+        }
+
+        public bool UpdatePedagog(Pedagog pedagog)
+        {
+            try
+            {
+                this.Connect();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "UPDATE pedagog " +
+                        "SET " +
+                        "lozinka = @lozinka, " +
+                        "ime = @ime, " +
+                        "prezime = @prezime " +
+                        "WHERE email = @email";
+                        
+                    command.CommandType = CommandType.Text;
+                    command.Parameters.AddWithValue("@lozinka", pedagog.Lozinka);
+                    command.Parameters.AddWithValue("@ime", pedagog.Ime);
+                    command.Parameters.AddWithValue("@prezime", pedagog.Prezime);                    
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+
+    }
+}
