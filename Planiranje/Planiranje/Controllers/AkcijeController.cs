d50b65b98045c1ae8865179f9b0fa270056e0edd
Igor Korpar <igor.korpar@student.mev.hr>
2018-06-22 15:33:12 +0200

    Akcije

--- Planiranje/Planiranje/Controllers/AkcijeController.cs
+++ Planiranje/Planiranje/Controllers/AkcijeController.cs
@@ -1,22 +1,191 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using Planiranje.Models;
+
 
 namespace Planiranje.Controllers
 {
     public class AkcijeController : Controller
     {
-        // GET: Akcije
-        public ActionResult Index()
+        private Aktivnost_akcija_DBHandle akcije_planovi = new Aktivnost_akcija_DBHandle();
+        int Page_No_Master = 1;
+        // GET: PodrucjaDjelovanja
+
+        public ActionResult Index(string Sort, string Search, string Filter, int? Page_No)
+
         {
-			if (PlaniranjeSession.Trenutni.PedagogId > 0)
-			{
-				ViewBag.Title = "Akcije";
-				return View();
-			}
-			return RedirectToAction("Prijava", "Planiranje");
-		}
+
+            if (PlaniranjeSession.Trenutni.PedagogId > 0)
+            {
+                ViewBag.Title = "Aktivnost akcije";
+                return View();
+            }
+            return RedirectToAction("Prijava", "Planiranje");
+        }
+        public ActionResult Index(string Sort, string Search, string Filter, int? Page_No)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            ViewBag.Title = "Pregled planova akcije";
+            ViewBag.CurrentSortOrder = Sort;
+            ViewBag.SortingName = String.IsNullOrEmpty(Sort) ? "Naziv" : "";
+
+            ViewBag.Message = "Grad";
+
+            ViewBag.FilterValue = Search;
+            if (Search != null)
+            {
+                Page_No = 1;
+            }
+            else
+            {
+                Search = Filter;
+            }
+            ViewBag.CurrentPage = 1;
+            if (Page_No != null)
+                ViewBag.CurrentPage = Page_No;
+
+
+            int Size_Of_Page = 10;
+            int No_Of_Page = (Page_No ?? 1);
+            if (Search == null || Search.Length == 0)
+            {
+
+                if (Request.IsAjaxRequest())
+                {
+                    int noP = (int)Page_No_Master;
+                    var Popis2 = akcije_planovi.ReadAktivnostAkcija().ToPagedList(No_Of_Page, Size_Of_Page);
+                    return PartialView("_GradView", Popis2);
+                }
+                Page_No_Master = No_Of_Page;
+                var Popis = akcije_planovi.ReadAktivnostAkcija().ToPagedList(No_Of_Page, Size_Of_Page);
+                return View(Popis);
+            }
+            else
+            {
+                Page_No_Master = No_Of_Page;
+                var Popis = akcije_planovi.ReadAktivnostAkcija(Search).ToPagedList(No_Of_Page, Size_Of_Page);
+                if (Request.IsAjaxRequest())
+                {
+                    return PartialView("_GradView", Popis);
+                }
+                return View(Popis);
+            }
+
+        }
+        public ActionResult NoviPlan()
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            if (Request.IsAjaxRequest())
+            {
+                ViewBag.IsUpdate = false;
+                return View("NoviPlan");
+            }
+            return View("NoviPlan");
+        }
+
+        [HttpPost]
+        public ActionResult NoviPlan(Aktivnost_akcija akt)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            Aktivnost_akcija akcija_plan = new Aktivnost_akcija();
+            akcija_plan.Id_pedagog = PlaniranjeSession.Trenutni.PedagogId;
+
+            akcija_plan.Naziv = akt.Naziv;
+            akcija_plan.Id_akcija = akt.Id_akcija;
+            akcija_plan.Id_aktivnost = akt.aktivnost;
+
+            if (akcija_plan.CreateAktivnostAkcija(akcija_plan))
+            {
+                TempData["alert"] = "<script>alert('Novi plan akcija je uspjesno spremljen!');</script>";
+            }
+            else
+            {
+                TempData["alert"] = "<script>alert('Novi plan akcija nije spremljen');</script>";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            Aktivnost_akcija akcija_plan = new Aktvnost_akcija();
+            akcija_plan = akcija_plan.ReadAktivnostAkcija(id);
+            if (Request.IsAjaxRequest())
+            {
+                ViewBag.IsUpdate = false;
+                return View("Uredi", akcija_plan);
+            }
+            return View("Uredi", akcija_plan);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Aktivnost_akcija akcija_plan)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            if (!akcija_plan.UpdateAktivnostAkcija(akcija_plan))
+            {
+                TempData["alert"] = "<script>alert('Plan akcije nije promjenjen!');</script>";
+            }
+            else
+            {
+                TempData["alert"] = "<script>alert('Plan akcije je uspjesno promjenjen!');</script>";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            Aktivnost_akcija akcija_plan = new Aktivnost_akcija();
+            akcija_plan = akcija_plan.ReadAktivnostAkcija(id);
+            if (Request.IsAjaxRequest())
+            {
+                ViewBag.IsUpdate = false;
+                return View("Obrisi", akcija_plan);
+            }
+            return View("Obrisi");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Aktivnost_akcija akcija_plan)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            if (!akcija_plan.DeleteAktivnostAkcija(akcija_plan.Id_god))
+            {
+                TempData["alert"] = "<script>alert('Plan akcije nije obrisan, dogodila se greska!');</script>";
+            }
+            else
+            {
+                TempData["alert"] = "<script>alert('Plan akcije je uspjesno obrisan!');</script>";
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Planiranje/Planiranje/Models/Aktivnost_akcija_DBHandle.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using Planiranje.Controllers;
+using PagedList;
+
+namespace Planiranje.Models
+{
+    public class Aktivnost_akcija_DBHandle
+    {
+        private MySqlConnection connection;
+
+        private void Connect()
+        {
+            string connection_string = ConfigurationManager.ConnectionStrings["BazaPodataka"].ConnectionString;
+            connection = new MySqlConnection(connection_string);
+        }
+
+        public List<Aktivnost_akcija> ReadAktivnostAkcija()
+        {
+
+            List<Aktivnost_akcija> aktivnost_akcija = new List<Aktivnost_akcija>();
+            this.Connect();
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT id_akcija, naziv,id_aktivnost " +
+                    "FROM aktivnost_akcija " +
+                    "ORDER BY id_aktivnost ASC";
+                //command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                connection.Open();
+                using (MySqlDataReader sdr = command.ExecuteReader())
+                {
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            Aktivnost_akcija akt = new Aktivnost_akcija()
+                            {
+
+                                Id_akcija = Convert.ToInt32(sdr["id_akcija"]),
+                                Naziv = sdr["naziv"].ToString(),
+                                Id_aktivnost = Convert.ToInt32(sdr["id_aktivnost"])
+                            };
+                            aktivnost_akcija.Add(akt);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return aktivnost_akcija;
+        }
+
+        public List<Aktivnost_akcija> ReadAktivnostAkcija(string search_string)
+        {
+            List<Aktivnost_akcija> aktivnost_akcija = new List<Aktivnost_akcija>();
+            this.Connect();
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT id_akcija, naziv,id_aktivnost " +
+                    "FROM aktivnost_akcija " +
+                    "WHERE naziv like '%" + search_string + "%' " +
+                    "ORDER BY id_aktivnost ASC";
+                command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                connection.Open();
+                using (MySqlDataReader sdr = command.ExecuteReader())
+                {
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            Aktivnost_akcija akt = new Aktivnost_akcija()
+                            {
+                                Id_akcija = Convert.ToInt32(sdr["id_akcija"]),
+                                Naziv = sdr["naziv"].ToString(),
+                                Id_aktivnost = Convert.ToInt32(sdr["id_aktivnost"])
+                            };
+                            aktivnost_akcija.Add(akt);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return aktivnost_akcija;
+        }
+
+        public Aktivnost_akcija ReadAktivnostAkcija(int _id)
+        {
+            Aktivnost_akcija akcija = new Aktivnost_akcija();
+            this.Connect();
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT id_akcija, naziv,id_aktivnost " +
+                    "FROM aktivnost_akcija " +
+                    "WHERE id_aktivnost = @id ";
+                command.CommandType = CommandType.Text;
+                command.Parameters.AddWithValue("@id", _id);
+                //command.Parameters.AddWithValue("@id_pedagog", PlaniranjeSession.Trenutni.PedagogId);
+                connection.Open();
+                using (MySqlDataReader sdr = command.ExecuteReader())
+                {
+                    if (sdr.HasRows)
+                    {
+                        while (sdr.Read())
+                        {
+                            akcija = new Aktivnost_akcija()
+                            {
+                                Id_akcija = Convert.ToInt32(sdr["id_akcija"]),
+                                Naziv = sdr["naziv"].ToString(),
+                                Id_aktivnost = Convert.ToInt32(sdr["id_aktivnost"])
+                            };
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return akcija;
+        }
+
+        public bool CreateAktivnostAkcija(Aktivnost_akcija akcija)
+        {
+            try
+            {
+                this.Connect();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "INSERT INTO aktivnost_akcija " +
+                        "(naziv) " +
+                        " VALUES (@naziv)";
+                    command.CommandType = CommandType.Text;
+                    command.Parameters.AddWithValue("@naziv", akcija.Naziv);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+
+        public bool UpdateAktivnostAkcija(Aktivnost_akcija akcija)
+        {
+            try
+            {
+                this.Connect();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "UPDATE aktivnost_akcija " +
+                        "SET " +
+                        "naziv = @naziv " +
+                        "WHERE id_podrucje = @id_aktivnost";
+                    command.CommandType = CommandType.Text;
+                    command.Parameters.AddWithValue("@id_akcija", akcija.Id_akcija);
+                    command.Parameters.AddWithValue("@naziv", akcija.Naziv);
+                    command.Parameters.AddWithValue("@id_aktivnost", akcija.Id_aktivnost);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+
+        public bool DeleteAktivnostAkcija(int id)
+        {
+            try
+            {
+                this.Connect();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    connection.Open();
+                    command.CommandText = "DELETE FROM aktivnost_akcija " +
+                        "WHERE id_akcija = @id_akcija";
+                    command.CommandType = CommandType.Text;
+                    command.Parameters.AddWithValue("@id_akcija", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return true;
+        }
+    }
+}
