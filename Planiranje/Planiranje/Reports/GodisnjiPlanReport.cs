c38bf934d3b117df51cee0539bda5b20ca229148
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-06-27 19:48:19 +0200

    added godisnji report

---
+++ Planiranje/Planiranje/Reports/GodisnjiPlanReport.cs
@@ -0,0 +1,108 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Planiranje.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Planiranje.Reports
+{
+    public class GodisnjiPlanReport
+    {
+        public byte[] Podaci { get; private set; }
+
+        public GodisnjiPlanReport(List<Godisnji_plan> godisnji_plan)
+        {
+            // generiranje pdf-a
+
+            // novi dokument, sa veličinom stranice i marginama
+            // mjere u iText# -> point = 1/72 inch
+            Document pdfDokument = new Document(
+                PageSize.A4, 50, 50, 20, 50);
+
+            MemoryStream memStream = new MemoryStream();
+            PdfWriter.GetInstance(pdfDokument, memStream).
+                CloseStream = false;
+
+            // otvaranje dokumenta
+            pdfDokument.Open();
+
+            // dodamo neki sadržaj za test
+            //pdfDokument.Add(new Paragraph("Test 123..."));
+
+            // fontovi
+            BaseFont font = BaseFont.CreateFont(BaseFont.HELVETICA,
+                BaseFont.CP1250, false);
+            Font header = new Font(font, 12, Font.NORMAL, BaseColor.DARK_GRAY);
+            Font naslov = new Font(font, 14, Font.BOLDITALIC, BaseColor.BLACK);
+            Font tekst = new Font(font, 10, Font.NORMAL, BaseColor.BLACK);
+
+            // logo
+            /*
+            var logo = iTextSharp.text.Image.GetInstance(
+                HostingEnvironment.MapPath("~/Content/MEV_LOGO.jpg"));
+            logo.Alignment = Element.ALIGN_LEFT;
+            logo.ScaleAbsoluteHeight(100);
+            logo.ScaleAbsoluteWidth(100);
+            pdfDokument.Add(logo);*/
+
+            // header
+            Paragraph p = new Paragraph("IZVJEŠTAJ", header);
+            pdfDokument.Add(p);
+
+            // naslov 
+            p = new Paragraph("Godisnji plan", naslov);
+            p.Alignment = Element.ALIGN_CENTER;
+            p.SpacingBefore = 30;
+            p.SpacingAfter = 30;
+            pdfDokument.Add(p);
+
+            // tablica sa popisom studenata
+            PdfPTable t = new PdfPTable(6); // 5 kolona
+            t.WidthPercentage = 100; // širina tablice
+            t.SetWidths(new float[] { 1, 2, 2, 2, 2, 3 });
+
+            // dodati zaglavlje
+            t.AddCell(VratiCeliju("R.br.", tekst, true, BaseColor.LIGHT_GRAY));
+            t.AddCell(VratiCeliju("Ak. godina", tekst, false, BaseColor.LIGHT_GRAY));
+            t.AddCell(VratiCeliju("Naziv", tekst, true, BaseColor.LIGHT_GRAY));
+			t.AddCell(VratiCeliju("Opis", tekst, true, BaseColor.LIGHT_GRAY));
+			t.AddCell(VratiCeliju("Opis", tekst, true, BaseColor.LIGHT_GRAY));
+			t.AddCell(VratiCeliju("Opis", tekst, true, BaseColor.LIGHT_GRAY));
+
+
+			// dodajemo popis studenata
+			int i = 1;
+            foreach (Godisnji_plan plan in godisnji_plan)
+            {
+                t.AddCell(VratiCeliju((i++).ToString(), tekst, true, BaseColor.WHITE));
+                t.AddCell(VratiCeliju(plan.Ak_godina, tekst, false, BaseColor.WHITE));
+                t.AddCell(VratiCeliju(plan.Br_radnih_dana.ToString(), tekst, true, BaseColor.WHITE));
+				t.AddCell(VratiCeliju(plan.Br_dana_godina_odmor.ToString(), tekst, true, BaseColor.WHITE));
+				t.AddCell(VratiCeliju(plan.Ukupni_rad_dana.ToString(), tekst, true, BaseColor.WHITE));
+				t.AddCell(VratiCeliju(plan.God_fond_sati.ToString(), tekst, true, BaseColor.WHITE));
+			}
+
+            // dodati tablicu na dokument
+            pdfDokument.Add(t);
+
+            // zatvaranje dokumenta
+            pdfDokument.Close();
+            Podaci = memStream.ToArray();
+        }
+
+        private PdfPCell VratiCeliju(string labela, Font font,
+            bool nowrap, BaseColor boja)
+        {
+            PdfPCell c1 = new PdfPCell(new Phrase(labela, font));
+            c1.BackgroundColor = boja;
+            c1.HorizontalAlignment = PdfPCell.ALIGN_LEFT;
+            c1.Padding = 5;
+            c1.NoWrap = nowrap;
+            return c1;
+        }
+    }
+}
