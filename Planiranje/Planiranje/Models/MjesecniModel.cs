d6c73e0e78aac990f271a8b4a80f9646fe884cdd
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-07-08 22:41:42 +0200

    added mjesecni model

---
+++ Planiranje/Planiranje/Models/MjesecniModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planiranje.Models
+{
+	public class MjesecniModel
+	{
+		public List<SelectListItem> GodisnjiPlanovi { get; set; }
+		public List<Mjesecni_plan> MjesecniPlanovi { get; set; }
+	}
+}
