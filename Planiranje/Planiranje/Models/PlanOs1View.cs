376508020933f73c26e949c53389d9a40d10a23f
David <david.sopar@student.mev.hr>
2018-07-21 22:15:03 +0200

    summary

--- Planiranje/Planiranje/Models/PlanOs1View.cs
+++ Planiranje/Planiranje/Models/PlanOs1View.cs
@@ -8,20 +8,49 @@ namespace Planiranje.Models
 {
     public class PlanOs1View
     {
+        /// <summary>
+        /// id plana
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// glavni model za os plan 1
+        /// </summary>
         public OS_Plan_1 OsPlan1 { get; set; }
+        /// <summary>
+        /// glavni model za os plan 1 područje
+        /// </summary>
         public OS_Plan_1_podrucje Podrucje { get; set; }
+        /// <summary>
+        /// lista os plan 1 područja
+        /// </summary>
         public List<OS_Plan_1_podrucje> OsPlan1Podrucje { get; set; }
+        /// <summary>
+        /// lista os plan 1 aktivnosti po područjima
+        /// </summary>
         public List<OS_Plan_1_aktivnost> OsPlan1Aktivnost { get; set; }
         public List<OS_Plan_1_akcija> OsPlan1Akcija { get; set; }
+        /// <summary>
+        /// lista svih ciljeva
+        /// </summary>
         public List<Ciljevi> Ciljevi { get; set; }
+        /// <summary>
+        /// lista svih područja djelovanja
+        /// </summary>
         public List<Podrucje_rada> PodrucjeRada { get; set; }
+        /// <summary>
+        /// lista svih aktivnosti
+        /// </summary>
         public List<Aktivnost> Aktivnosti { get; set; }
-        
+        /// <summary>
+        /// lista svih područja za dropdown list
+        /// </summary>
         public IEnumerable<SelectListItem> PodrucjeItems
         {
             get { return new SelectList(PodrucjeRada, "Id_podrucje", "Naziv"); }
         }
+        /// <summary>
+        /// lista svih ciljeva za dropdown list
+        /// </summary>
         public IEnumerable<SelectListItem> CiljeviItems
         {
             get { return new SelectList(Ciljevi, "Id_cilj", "Naziv"); }
