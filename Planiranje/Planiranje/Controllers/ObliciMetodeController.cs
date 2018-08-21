15580be849893f57fa69f25af4d2172bf738d5bf
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-08-21 11:43:45 +0200

    added ajax for obliciMetode

--- Planiranje/Planiranje/Controllers/ObliciMetodeController.cs
+++ Planiranje/Planiranje/Controllers/ObliciMetodeController.cs
@@ -14,60 +14,18 @@ namespace Planiranje.Controllers
     public class ObliciMetodeController : Controller
     {
         private Oblici_DBHandle oblici = new Oblici_DBHandle();
-        int Page_No_Master = 1;
-        // GET: ObliciMetode
-        public ActionResult Index(string Sort, string Search, string Filter, int? Page_No)
+
+        public ActionResult Index()
         {
             if (PlaniranjeSession.Trenutni.PedagogId <= 0)
             {
                 return RedirectToAction("Index", "Planiranje");
             }
             ViewBag.Title = "Pregled oblika i metoda";
-            ViewBag.CurrentSortOrder = Sort;
-            ViewBag.SortingName = String.IsNullOrEmpty(Sort) ? "Naziv" : "";
 
-            ViewBag.Message = "Grad";
-
-            ViewBag.FilterValue = Search;
-            if (Search != null)
-            {
-                Page_No = 1;
-            }
-            else
-            {
-                Search = Filter;
-            }
-            ViewBag.CurrentPage = 1;
-            if (Page_No != null)
-                ViewBag.CurrentPage = Page_No;
-
-
-            int Size_Of_Page = 10;
-            int No_Of_Page = (Page_No ?? 1);
-            if (Search == null || Search.Length == 0)
-            {
-
-                if (Request.IsAjaxRequest())
-                {
-                    int noP = (int)Page_No_Master;
-                    var Popis2 = oblici.ReadOblici().ToPagedList(No_Of_Page, Size_Of_Page);
-                    return PartialView("_GradView", Popis2);
-                }
-                Page_No_Master = No_Of_Page;
-                var Popis = oblici.ReadOblici().ToPagedList(No_Of_Page, Size_Of_Page);
-                return View(Popis);
-            }
-            else
-            {
-                Page_No_Master = No_Of_Page;
-                var Popis = oblici.ReadOblici(Search).ToPagedList(No_Of_Page, Size_Of_Page);
-                if (Request.IsAjaxRequest())
-                {
-                    return PartialView("_GradView", Popis);
-                }
-
-                return View(Popis);
-            }
+			ObliciMetodeModel model = new ObliciMetodeModel();
+			model.oblici = oblici.ReadOblici();
+			return View("Index", model);
         }
 
         public ActionResult NoviPlan()
@@ -78,34 +36,27 @@ namespace Planiranje.Controllers
             }
             if (Request.IsAjaxRequest())
             {
-                ViewBag.IsUpdate = false;
-                return View("NoviPlan");
+				ObliciMetodeModel model = new ObliciMetodeModel();
+                return View("NoviPlan", model);
             }
-            return View("NoviPlan");
+			return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public ActionResult NoviPlan(Oblici oblik)
+        public ActionResult NoviPlan(ObliciMetodeModel model)
         {
             if (PlaniranjeSession.Trenutni.PedagogId <= 0)
             {
                 return RedirectToAction("Index", "Planiranje");
             }
-            //Mjesecni_plan mjesecni_plan = new Mjesecni_plan();
-            //mjesecni_plan.ID_pedagog = PlaniranjeSession.Trenutni.PedagogId;
-            //mjesecni_plan.Ak_godina = _mjesecni_plan.Ak_godina;
-            //mjesecni_plan.Naziv = _mjesecni_plan.Naziv;
-            //mjesecni_plan.Opis = _mjesecni_plan.Opis;
-            
-            if (oblici.CreateOblici(oblik))
+            if (model.oblik.Naziv != null && oblici.CreateOblici(model.oblik))
             {
-                TempData["alert"] = "<script>alert('Novi oblik je uspjesno spremljen!');</script>";
-            }
+				return RedirectToAction("Index");
+			}
             else
             {
-                TempData["alert"] = "<script>alert('Novi oblik nije spremljen');</script>";
-            }
-            return RedirectToAction("Index");
+				return View("NoviPlan", model);
+			}
         }
 
         public ActionResult Edit(int id)
@@ -114,32 +65,30 @@ namespace Planiranje.Controllers
             {
                 return RedirectToAction("Index", "Planiranje");
             }
-            Oblici oblik = new Oblici();
-            oblik = oblici.ReadOblici(id);
             if (Request.IsAjaxRequest())
             {
-                ViewBag.IsUpdate = false;
-                return View("Uredi", oblik);
+				ObliciMetodeModel model = new ObliciMetodeModel();
+				model.oblik = oblici.ReadOblici(id);
+                return View("Uredi", model);
             }
-            return View("Uredi", oblik);
-        }
+			return RedirectToAction("Index");
+		}
 
         [HttpPost]
-        public ActionResult Edit(Oblici oblik)
+        public ActionResult Edit(ObliciMetodeModel model)
         {
             if (PlaniranjeSession.Trenutni.PedagogId <= 0)
             {
                 return RedirectToAction("Index", "Planiranje");
             }
-            if (!oblici.UpdateOblici(oblik))
+            if (model.oblik.Naziv != null && oblici.UpdateOblici(model.oblik))
             {
-                TempData["alert"] = "<script>alert('Oblik nije promjenjen!');</script>";
-            }
+				return RedirectToAction("Index");
+			}
             else
             {
-                TempData["alert"] = "<script>alert('Oblik je uspjesno promjenjen!');</script>";
-            }
-            return RedirectToAction("Index");
+				return View("Uredi", model);
+			}
         }
 
         public ActionResult Delete(int id)
@@ -148,32 +97,31 @@ namespace Planiranje.Controllers
             {
                 return RedirectToAction("Index", "Planiranje");
             }
-            Oblici oblik = new Oblici();
-            oblik = oblici.ReadOblici(id);
+
             if (Request.IsAjaxRequest())
-            {
-                ViewBag.IsUpdate = false;
-                return View("Obrisi", oblik);
+			{
+				ObliciMetodeModel model = new ObliciMetodeModel();
+				model.oblik = oblici.ReadOblici(id);
+				return View("Obrisi", model);
             }
-            return View("Obrisi");
-        }
+			return RedirectToAction("Index");
+		}
 
         [HttpPost]
-        public ActionResult Delete(Oblici oblik)
+        public ActionResult Delete(ObliciMetodeModel model)
         {
             if (PlaniranjeSession.Trenutni.PedagogId <= 0)
             {
                 return RedirectToAction("Index", "Planiranje");
             }
-            if (!oblici.DeleteOblici(oblik.Id_oblici))
+            if (!oblici.DeleteOblici(model.oblik.Id_oblici))
             {
-                TempData["alert"] = "<script>alert('Oblik nije obrisan, dogodila se greska!');</script>";
-            }
+				return View("Obrisi", model);
+			}
             else
             {
-                TempData["alert"] = "<script>alert(Oblik je uspjesno obrisan!');</script>";
-            }
-            return RedirectToAction("Index");
+				return RedirectToAction("Index");
+			}
         }
     }
 }
