eff01d783a36a1d39d22a44dd2152872de9b0d00
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-09-01 18:43:57 +0200

    fixed session timeout redirection to login page

--- Planiranje/Planiranje/Scripts/custom.js
+++ Planiranje/Planiranje/Scripts/custom.js
@@ -9,7 +9,11 @@ function reloadPage(path) {
 	$.ajax({
 		url: path,
 		success: function (data) {
-			$('#content').html(data);
+			if ($(data)[1].tagName === "META") {
+				location.reload();
+			} else {
+				$('#content').html(data);
+			}
 		},
 		error: function (xhr, ajaxOptions, thrownError) {
 			alert(xhr.responseText);
@@ -21,8 +25,12 @@ function showModal(path) {
 	$.ajax({
 		url: path,
 		success: function (data) {
-			$('#modalContainer').html(data);
-			$('#modal').modal('show');
+			if ($(data)[1].tagName === "META") {
+				location.reload();
+			} else {
+				$('#modalContainer').html(data);
+				$('#modal').modal('show');
+			}
 		},
 		error: function (xhr, ajaxOptions, thrownError) {
 			alert(xhr.responseText);
@@ -37,9 +45,13 @@ function reloadWithData(path, id, message) {
 		type: "POST",
 		data: dt,
 		success: function (data) {
-			$("#content").html(data);
-			if ($(data)[0].id !== "newPlan") {
-				showSnackBar(message);
+			if ($(data)[1].tagName === "META") {
+				location.reload();
+			} else {
+				$("#content").html(data);
+				if ($(data)[0].id !== "newPlan") {
+					showSnackBar(message);
+				}
 			}
 		},
 		error: function (request, status, error) {
@@ -50,7 +62,7 @@ function reloadWithData(path, id, message) {
 
 function hideModalA(event, path, id, message) {
 	if (event.type === "keydown") {
-		if (event.keyCode == 13) {
+		if (event.keyCode === 13) {
 			event.preventDefault();
 			hideModal(path, id, message);
 		}
@@ -64,16 +76,20 @@ function hideModal(path, id, message) {
 		type: "POST",
 		data: dt,
 		success: function (data) {
-			if ($(data)[0].className === "modal-dialog") {
-				$("#modalContainer").html(data);
-				$('#newName').focus();
-			} else if ($(data)[0].className === "inner") {
-				$('#modal').modal('hide');
-				$('#modalContainer').removeData();
-				$('.modal-backdrop').remove();
-				$("#content").html(data);
-				if (message !== null) {
-					showSnackBar(message);
+			if ($(data)[1].tagName === "META") {
+				location.reload();
+			} else {
+				if ($(data)[0].className === "modal-dialog") {
+					$("#modalContainer").html(data);
+					$('#newName').focus();
+				} else if ($(data)[0].className === "inner") {
+					$('#modal').modal('hide');
+					$('#modalContainer').removeData();
+					$('.modal-backdrop').remove();
+					$("#content").html(data);
+					if (message !== null) {
+						showSnackBar(message);
+					}
 				}
 			}
 		},
