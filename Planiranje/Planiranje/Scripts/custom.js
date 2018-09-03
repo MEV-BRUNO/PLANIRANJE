0401b71857cd5614a0cd1c1f8b6545c1f51b8c2e
Sebastijan Sakac <sebastijan.sakac@student.mev.hr>
2018-09-03 12:38:37 +0200

    fixed showing scrollbar while modal is shown

--- Planiranje/Planiranje/Scripts/custom.js
+++ Planiranje/Planiranje/Scripts/custom.js
@@ -153,4 +153,13 @@ $(document).ready(function () {
 			}
 		});
 	});
+	$('.modal').on('show.bs.modal', function () {
+		if ($(document).height() > $(window).height()) {
+			// no-scroll
+			$('body').addClass("modal-open-noscroll");
+		}
+		else {
+			$('body').removeClass("modal-open-noscroll");
+		}
+	});
 });
