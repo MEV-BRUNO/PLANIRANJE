b07329cc9095294c580feec02b51edf8e668573c
David <david.sopar@student.mev.hr>
2018-11-29 22:10:23 +0100

    plan os1 - validacija brojeva poboljšana-> ne dozvoljava upis negativne vrijednosti

--- Planiranje/Planiranje/Controllers/MjesecniPlanController.cs
+++ Planiranje/Planiranje/Controllers/MjesecniPlanController.cs
@@ -206,7 +206,7 @@ namespace Planiranje.Controllers
             }
             DateTime date = new DateTime(1, 1, 1, 0, 0, 0);
             if (model.mjesecniDetalj.Aktivnost == null||model.mjesecniDetalj.Subjekti==null||model.mjesecniDetalj.Suradnici==null
-                || model.mjesecniDetalj.Podrucje==null||model.mjesecniDetalj.Vrijeme.CompareTo(date)==0||model.mjesecniDetalj.Br_sati==0
+                || model.mjesecniDetalj.Podrucje==null||model.mjesecniDetalj.Vrijeme.CompareTo(date)==0||model.mjesecniDetalj.Br_sati<0
                 || model.mjesecniDetalj.Biljeska==null)
             {
                 model.Aktivnosti = aktivnosti.ReadAktivnost();
@@ -259,7 +259,7 @@ namespace Planiranje.Controllers
                 return RedirectToAction("Index", "Planiranje");
             }
             if (model.mjesecniDetalj.Aktivnost == null || model.mjesecniDetalj.Subjekti == null || model.mjesecniDetalj.Suradnici == null
-                || model.mjesecniDetalj.Podrucje == null || model.mjesecniDetalj.Vrijeme == null || model.mjesecniDetalj.Br_sati == 0
+                || model.mjesecniDetalj.Podrucje == null || model.mjesecniDetalj.Vrijeme == null || model.mjesecniDetalj.Br_sati < 0
                 || model.mjesecniDetalj.Biljeska == null)
             {
                 model.Aktivnosti = aktivnosti.ReadAktivnost();
--- Planiranje/Planiranje/Models/OS_Plan_1_aktivnost.cs
+++ Planiranje/Planiranje/Models/OS_Plan_1_aktivnost.cs
@@ -23,43 +23,42 @@ namespace Planiranje.Models
         [DisplayName("Potrebno sati")]
         public string Potrebno_sati { get; set; }
         [Required]
-        public int Br_sati { get; set; }
-		[Required]
+        public int Br_sati { get; set; }        
         [DisplayName("Siječanj")]
-        public int Mj_1 { get; set; }
-		[Required]
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
+        public int Mj_1 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Veljača")]
-        public int Mj_2 { get; set; }
-		[Required]
+        public int Mj_2 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Ožujak")]
-        public int Mj_3 { get; set; }
-		[Required]
+        public int Mj_3 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Travanj")]
-        public int Mj_4 { get; set; }
-		[Required]
+        public int Mj_4 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Svibanj")]
-        public int Mj_5 { get; set; }
-		[Required]
+        public int Mj_5 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Lipanj")]
-        public int Mj_6 { get; set; }
-		[Required]
+        public int Mj_6 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Srpanj")]
-        public int Mj_7 { get; set; }
-		[Required]
+        public int Mj_7 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Kolovoz")]
-        public int Mj_8 { get; set; }
-		[Required]
+        public int Mj_8 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Rujan")]
-        public int Mj_9 { get; set; }
-		[Required]
+        public int Mj_9 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Listopad")]
-        public int Mj_10 { get; set; }
-		[Required]
+        public int Mj_10 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Studeni")]
-        public int Mj_11 { get; set; }
-		[Required]
+        public int Mj_11 { get; set; }		
+        [Range(0, Int32.MaxValue, ErrorMessage = "Vrijednost mora biti jednaka ili veća od 0")]
         [DisplayName("Prosinac")]
         public int Mj_12 { get; set; }
-
 	}
 }
