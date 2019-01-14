5501aedfd37e9d476211ff8b61531f178fadb735
David <david.sopar@student.mev.hr>
2019-01-14 22:07:01 +0100

    update select razred -> dodan option 'svi'

--- Planiranje/Planiranje/Controllers/PracenjeUcenikaController.cs
+++ Planiranje/Planiranje/Controllers/PracenjeUcenikaController.cs
@@ -33,16 +33,25 @@ namespace Planiranje.Controllers
             razredi = baza.RazredniOdjel.Where(w => w.Id_skola == PlaniranjeSession.Trenutni.OdabranaSkola && w.Sk_godina == godina).ToList();
             return View(razredi);
         }
-        public ActionResult OdabirUcenika (int razred)
+        public ActionResult OdabirUcenika (int razred, int godina)
         {
             if (!Request.IsAjaxRequest())
             {
                 return RedirectToAction("Index", "Planiranje");
-            }
+            }            
             RazredniOdjel razredniOdjel = baza.RazredniOdjel.SingleOrDefault(s => s.Id == razred && s.Id_skola==PlaniranjeSession.Trenutni.OdabranaSkola);
             List<Ucenik> ucenici = new List<Ucenik>();
             if (razredniOdjel == null)
             {
+                if (razred != -1)
+                {
+                    return View(ucenici);
+                }
+                ucenici = (from raz in baza.RazredniOdjel
+                           join ur in baza.UcenikRazred on raz.Id equals ur.Id_razred
+                           join uc in baza.Ucenik on ur.Id_ucenik equals uc.Id_ucenik
+                           where raz.Sk_godina == godina
+                           select uc).ToList();
                 return View(ucenici);
             }
             ucenici = (from ur in baza.UcenikRazred
