e99732fc3872d76840bdd958a405d125f5eb1e27
David <david.sopar@student.mev.hr>
2019-02-18 02:52:06 +0100

    započeti roditelji: bilješka o radu s roditeljem (RoditeljBiljeska)->kreiran controller, 2 view-a

---
+++ Planiranje/Planiranje/Controllers/RoditeljBiljeskaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Planiranje.Models.Ucenici;
+using Planiranje.Models;
+using System.Net;
+using Planiranje.Reports;
+using System.IO;
+
+namespace Planiranje.Controllers
+{
+    public class RoditeljBiljeskaController : Controller
+    {
+        private BazaPodataka baza = new BazaPodataka();
+        // GET: RoditeljBiljeska
+        public ActionResult Index()
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            ViewBag.godine = baza.SkolskaGodina.ToList();
+            return View();
+        }
+        public ActionResult Detalji(int id, int godina)
+        {
+            //ulazni parametar id je id učenika
+            return View();
+        }
+    }
+}
