e5400274cc586a78f37c9c8d66c540dce123bfeb
David <david.sopar@student.mev.hr>
2019-02-24 10:11:11 +0100

    roditelj procjena: kreiran controller, index stranica

---
+++ Planiranje/Planiranje/Controllers/RoditeljProcjenaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Planiranje.Models.Ucenici;
+using Planiranje.Models;
+using System.Net;
+using Planiranje.Reports;
+using System.IO;
+
+namespace Planiranje.Controllers
+{
+    public class RoditeljProcjenaController : Controller
+    {
+        private BazaPodataka baza = new BazaPodataka();
+        // GET: RoditeljProcjena
+        public ActionResult Index()
+        {
+            List<Sk_godina> godine = baza.SkolskaGodina.ToList();
+            ViewBag.godine = godine;
+            return View();
+        }
+    }
+}
