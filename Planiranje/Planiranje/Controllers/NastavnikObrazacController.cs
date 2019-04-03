881f9f8a0e5cc0525157fba031d625ac70b367da
David <david.sopar@student.mev.hr>
2019-04-03 21:14:15 +0200

    nastavnik obrazac: kreiran index i detalji view; controller

---
+++ Planiranje/Planiranje/Controllers/NastavnikObrazacController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Planiranje.Models.Ucenici;
+using Planiranje.Models;
+using System.Net;
+using Planiranje.Reports;
+using System.IO;
+
+namespace Planiranje.Controllers
+{
+    public class NastavnikObrazacController : Controller
+    {
+        private BazaPodataka baza = new BazaPodataka();
+        // GET: NastavnikObrazac
+        public ActionResult Index()
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            ViewBag.godine = baza.SkolskaGodina.ToList();
+            return View();
+        }
+        public ActionResult Detalji(int id, int godina)
+        {
+            //ulazni parametar id je id nastavnika, a godina je skolska godina
+            if (!Request.IsAjaxRequest() || PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            List<Nastavnik_obrazac> model = baza.NastavnikObrazac.Where(w => w.Id_nastavnik == id && w.Id_pedagog == PlaniranjeSession.Trenutni.PedagogId &&
+            w.Sk_godina == godina && w.Id_skola == PlaniranjeSession.Trenutni.OdabranaSkola).ToList();
+            Nastavnik nastavnik = baza.Nastavnik.SingleOrDefault(s => s.Id == id && s.Id_skola == PlaniranjeSession.Trenutni.OdabranaSkola);
+            ViewBag.nastavnik = nastavnik;
+            ViewBag.godina = godina;
+            ViewBag.listaodjela = baza.RazredniOdjel.Where(w => w.Id_skola == PlaniranjeSession.Trenutni.PedagogId &&
+            w.Sk_godina == godina).ToList();
+            return View(model);            
+        }
+    }
+}
