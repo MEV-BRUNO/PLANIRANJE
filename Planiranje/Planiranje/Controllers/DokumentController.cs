4cd72d1162f9903e95be0059303af6f434265b8a
David <david.sopar@student.mev.hr>
2019-04-15 20:47:41 +0200

    dokumenti: spremanje dokumenata

--- Planiranje/Planiranje/Controllers/DokumentController.cs
+++ Planiranje/Planiranje/Controllers/DokumentController.cs
@@ -25,5 +25,60 @@ namespace Planiranje.Controllers
             w.Id_skola == PlaniranjeSession.Trenutni.OdabranaSkola).ToList();
             return View(dokumenti);
         }
+        public ActionResult NoviDokument()
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0 || !Request.IsAjaxRequest())
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            if(Request.Files.Count==0)
+            {
+                ViewBag.greska = "Nema datoteke";
+                return View("NoviDokument");
+            }
+            try
+            {
+                var file = Request.Files[0];
+
+                string ekstenzija = Path.GetExtension(file.FileName);
+                if (ekstenzija.CompareTo(".exe") == 0 || ekstenzija.CompareTo(".bin") == 0)
+                {
+                    ViewBag.greska = "Datoteke *.exe i *.bin nisu podržane";
+                    return View("NoviDokument");
+                }
+                string direktorij = Server.MapPath("~/Dokumenti/" + PlaniranjeSession.Trenutni.PedagogId.ToString());
+                if (!Directory.Exists(direktorij))
+                {
+                    Directory.CreateDirectory(direktorij);
+                }
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(direktorij, fileName);
+                file.SaveAs(path);
+                //datoteka je spremljena, slijedi upis u bazu podataka
+                Dokument dokument = new Dokument();
+                dokument.Id_pedagog = PlaniranjeSession.Trenutni.PedagogId;
+                dokument.Id_skola = PlaniranjeSession.Trenutni.OdabranaSkola;
+                dokument.Naziv = fileName;
+                using(var db = new BazaPodataka())
+                {
+                    db.Dokument.Add(dokument);
+                    db.SaveChanges();
+                }                
+            }
+            catch(Exception e)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
