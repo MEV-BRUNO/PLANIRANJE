b2c30407fdf2f3708375dcf280cc2fdf08ba952d
David <david.sopar@student.mev.hr>
2019-04-28 16:19:01 +0200

    ucenik zapisnik: model, osobni podaci

--- Planiranje/Planiranje/Controllers/UcenikZapisnikController.cs
+++ Planiranje/Planiranje/Controllers/UcenikZapisnikController.cs
@@ -23,6 +23,66 @@ namespace Planiranje.Controllers
             }
             ViewBag.godine = baza.SkolskaGodina.ToList();
             return View();
+        }        
+        public ActionResult Detalji (int id, int godina)
+        {
+            //ulazni parametar id je id učenika
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            Ucenik ucenik = baza.Ucenik.SingleOrDefault(s => s.Id_ucenik == id);
+            if (ucenik == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(ucenik);
+        }        
+        public ActionResult Osnovni (int id)
+        {
+            if (PlaniranjeSession.Trenutni.PedagogId <= 0)
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            //id učenika
+            Ucenik ucenik = baza.Ucenik.SingleOrDefault(s => s.Id_ucenik == id);
+            ViewBag.odjeli = (from raz in baza.RazredniOdjel join ur in baza.UcenikRazred on raz.Id equals ur.Id_razred
+                              join uc in baza.Ucenik on ur.Id_ucenik equals uc.Id_ucenik where raz.Id_skola==PlaniranjeSession.Trenutni.OdabranaSkola
+                              && uc.Id_ucenik==id select raz).ToList();
+            return View(ucenik);
+        }
+        [HttpPost]
+        public ActionResult Osnovni (Ucenik model)
+        {
+            if(PlaniranjeSession.Trenutni.PedagogId<=0 || !Request.IsAjaxRequest())
+            {
+                return RedirectToAction("Index", "Planiranje");
+            }
+            using (var db = new BazaPodataka())
+            {
+                try
+                {
+                    int id = model.Id_ucenik;
+                    var result = db.Ucenik.SingleOrDefault(s => s.Id_ucenik == id);
+                    if (result != null)
+                    {
+                        result.Datum = model.Datum;
+                        result.Oib = model.Oib;
+                        result.Adresa = model.Adresa;
+                        result.Grad = model.Grad;
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+                    }
+                }
+                catch
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+                }
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
     }
 }
---
+++ Planiranje/Planiranje/Models/Ucenici/Ucenik_zapisnik.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Planiranje.Models.Ucenici
+{
+    public class Ucenik_zapisnik
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Id_ucenik_razred { get; set; }
+        public int Id_pedagog { get; set; }
+        public string Razlog { get; set; }
+        public int Odgojni_utjecaj_majka { get; set; }
+        public int Odgojni_utjecaj_otac { get; set; }
+        
+        public string Procjena_statusa_obitelji { get; set; }
+        public int Odnos_prema_ucenju_majka { get; set; }
+        public int Odnos_prema_ucenju_otac { get; set; }
+        
+        public int Suradnja_roditelja_majka { get; set; }
+        public int Suradnja_roditelja_otac { get; set; }
+        
+        public string Odnos_s_prijateljima { get; set; }
+        public string Kako_provodi_slobodno_vrijeme { get; set; }
+        public string Procjena_mogucih_losih_utjecaja { get; set; }
+        public string Zdravstvene_poteskoce_ucenika { get; set; }
+        public string Podaci_o_naglim_promjenama { get; set; }
+        public string Izrecene_pedagoske_mjere { get; set; }
+    }
+}
