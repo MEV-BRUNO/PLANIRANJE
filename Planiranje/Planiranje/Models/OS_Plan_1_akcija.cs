2ff0d25f25950d605e4e32bf187f5dced21cde4d
David Sopar <david.sopar@student.mev.hr>
2019-05-08 13:48:52 +0200

    update plan os 1: display name atributa za akcije

--- Planiranje/Planiranje/Models/OS_Plan_1_akcija.cs
+++ Planiranje/Planiranje/Models/OS_Plan_1_akcija.cs
@@ -13,44 +13,58 @@ namespace Planiranje.Models
         public int Id { get; set; }        
         public int Id_aktivnost { get; set; }        
         public int Red_br_akcija { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Obavezno polje")]
+        [DisplayName("Opis akcije")]
         public string Opis_akcija { get; set; }
-        [Required(ErrorMessage = "Obavezno polje")]        
+        [Required(ErrorMessage = "Obavezno polje")]    
+        [DisplayName("Potrebno sati")]
         public string Potrebno_sati { get; set; }        
         public int Br_sati { get; set; }
+        [DisplayName("Siječanj")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_1 { get; set; }
+        [DisplayName("Veljača")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_2 { get; set; }
+        [DisplayName("Ožujak")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_3 { get; set; }
+        [DisplayName("Travanj")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_4 { get; set; }
+        [DisplayName("Svibanj")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_5 { get; set; }
+        [DisplayName("Lipanj")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_6 { get;set;}
+        [DisplayName("Srpanj")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_7 { get; set; }
+        [DisplayName("Kolovoz")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_8 { get; set; }
+        [DisplayName("Rujan")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_9 { get; set; }
+        [DisplayName("Listopad")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_10 { get; set; }
+        [DisplayName("Studeni")]
         [Required(ErrorMessage = "Obavezno polje")]
         [Range(0, int.MaxValue, ErrorMessage = "Vrijednost mora biti veća od 0")]
         public int Mj_11 { get; set; }
+        [DisplayName("Prosinac")]
         [Required(ErrorMessage ="Obavezno polje")]
         [Range(0,int.MaxValue,ErrorMessage ="Vrijednost mora biti veća od 0")]
         public int Mj_12 { get; set; }
