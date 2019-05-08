9d002133c595cb7ea45d09af0d2c42909179908b
David <david.sopar@student.mev.hr>
2019-05-08 02:42:35 +0200

    update os plan 1: ponovno kreiran model za područje

--- Planiranje/Planiranje/Models/OS_Plan_1_Podrucje.cs
+++ Planiranje/Planiranje/Models/OS_Plan_1_Podrucje.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,11 +9,11 @@ namespace Planiranje.Models
 {
     public class OS_Plan_1_podrucje
     {
-        [Key]        
-        public int Id_plan { get; set; }        
-        public int Id_glavni_plan { get; set; }        
+        [Key]
+        public int Id_plan { get; set; }
+        public int Id_glavni_plan { get; set; }
         public int Red_br_podrucje { get; set; }
-        [Required(ErrorMessage ="Ovo je obavezno polje")]
+        [Required(ErrorMessage = "Ovo je obavezno polje")]
         [DisplayName("Opis područja")]
         public int Opis_Podrucje { get; set; }
         [Required(ErrorMessage = "Ovo je obavezno polje")]
@@ -24,30 +24,29 @@ namespace Planiranje.Models
         public string Cilj { get; set; }
         [Required]
         public int Br_sati { get; set; }
-		[Required]
-		public int Mj_1 { get; set; }
-		[Required]
-		public int Mj_2 { get; set; }
-		[Required]
-		public int Mj_3 { get; set; }
-		[Required]
-		public int Mj_4 { get; set; }
-		[Required]
-		public int Mj_5 { get; set; }
-		[Required]
-		public int Mj_6 { get; set; }
-		[Required]
-		public int Mj_7 { get; set; }
-		[Required]
-		public int Mj_8 { get; set; }
-		[Required]
-		public int Mj_9 { get; set; }
-		[Required]
-		public int Mj_10 { get; set; }
-		[Required]
-		public int Mj_11 { get; set; }
-		[Required]
-		public int Mj_12 { get; set; }
-
-	}
+        [Required]
+        public int Mj_1 { get; set; }
+        [Required]
+        public int Mj_2 { get; set; }
+        [Required]
+        public int Mj_3 { get; set; }
+        [Required]
+        public int Mj_4 { get; set; }
+        [Required]
+        public int Mj_5 { get; set; }
+        [Required]
+        public int Mj_6 { get; set; }
+        [Required]
+        public int Mj_7 { get; set; }
+        [Required]
+        public int Mj_8 { get; set; }
+        [Required]
+        public int Mj_9 { get; set; }
+        [Required]
+        public int Mj_10 { get; set; }
+        [Required]
+        public int Mj_11 { get; set; }
+        [Required]
+        public int Mj_12 { get; set; }
+    }
 }
