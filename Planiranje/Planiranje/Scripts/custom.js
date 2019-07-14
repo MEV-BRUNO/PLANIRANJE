dcc834156899c09d1cd1e41d7767d34316d2f399
David Sopar <david.sopar@student.mev.hr>
2019-07-14 17:22:51 +0200

    update navigacija: promjena naziva

--- Planiranje/Planiranje/Scripts/custom.js
+++ Planiranje/Planiranje/Scripts/custom.js
@@ -2,7 +2,7 @@
 	var x = document.getElementById("snackbar");
 	x.innerHTML = message;
 	x.className = "show";
-	setTimeout(function () {x.className = x.className.replace("show", ""); }, 3000);
+	setTimeout(function () {x.className = x.className.replace("show", ""); }, 6000);
 }
 
 function showModal(path) {
